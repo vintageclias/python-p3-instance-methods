4f49b116819dbe0abd771a537a5d3c0a91e44d56
vintageclias <vintageclias@gmail.com>
2025-02-20 13:23:56 -0800

    instance method

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,11 @@
 
 class Person:
     # Class body goes here
+     def talk(self):  
+        print("Hello World!")  
 
+    # Adding the walk() method  
+     def walk(self):  
+        print("The person is walking.") 
     #Instance method definition
-    pass
+     pass
